a5f79ac4b7b52c0dc2ee1549c2fbe8c28d88a097
Eric <ejwinebrenner@gmail.com>
2020-01-21 10:00:07 -0500

    Updates

---
+++ precache-manifest.5589edd5dfc466a6f54384a0d5f036de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702e510cb14c3c26110e2b3d0e7373f1",
+    "url": "/monsters-rolodex-deployed/index.html"
+  },
+  {
+    "revision": "1f9f955d8f33b04f2301",
+    "url": "/monsters-rolodex-deployed/static/css/main.e562d9f6.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex-deployed/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-deployed/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f9f955d8f33b04f2301",
+    "url": "/monsters-rolodex-deployed/static/js/main.7a2f66a1.chunk.js"
+  },
+  {
+    "revision": "5cc840e50df8aced7e99",
+    "url": "/monsters-rolodex-deployed/static/js/runtime-main.923eb359.js"
+  }
+]);
